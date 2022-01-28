22ba457dec3edcc58fae1e11b6160990393816d3
kiri0314 <t.kirihara0314@gmail.com>
2022-01-28 23:27:18 +0900

    upgrade: Add constructor

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,22 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 16:57:10 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/28 16:57:11 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/28 23:26:58 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->fixed_point = 0;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
 
