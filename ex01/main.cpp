71cfcead753460fd3a22391f5b0227673503fdcd
kiri0314 <t.kirihara0314@gmail.com>
2022-01-29 17:00:04 +0900

    upgrade: Add testcase

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 16:56:29 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/29 00:40:43 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/29 01:03:21 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,19 @@
 int	main()
 {
 	Fixed a;
-	Fixed b( a );
-	Fixed c;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-	c = b;
+	std::cout << "a is" << a << std::endl;
+	std::cout << "b is" << b << std::endl;
+	std::cout << "c is" << c << std::endl;
+	std::cout << "d is" << d << std::endl;
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a is" << a.toInt() << "as integer" << std::endl;
+	std::cout << "b is" << b.toInt() << "as integer" << std::endl;
+	std::cout << "c is" << c.toInt() << "as integer" << std::endl;
+	std::cout << "d is" << d.toInt() << "as integer" << std::endl;
 
 	return 0;
 }
